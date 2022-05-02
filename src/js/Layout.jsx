fadfec0fbfb673a7e7afff7d97ee23001b0f49a6
itrytocodethings <imwaynebailey@gmail.com>
2022-05-01 21:06:13 -0400

    started on editor page, absolute WIP

--- src/js/Layout.jsx
+++ src/js/Layout.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { NoteApp } from "./Pages/NoteApp";
+import NoteEditor from "./Pages/NoteEditor";
 
 const Layout = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<NoteApp />}></Route>
-        <Route path="/editor" element={<NoteApp />}></Route>
+        <Route path="/editor" element={<NoteEditor />}></Route>
       </Routes>
     </BrowserRouter>
   )
--- src/js/Pages/NoteEditor.jsx
+++ src/js/Pages/NoteEditor.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import SideBar from "../Components/Sidebar";
+import MainContent from "../Components/MainContent";
+
+import '../../assets/css/editor.css'
+import {IoArrowBack} from 'react-icons/io5';
 
 const NoteEditor = () => {
-  return (<div>Note Editor</div>);
-}
+  return (
+    <div className="app-container">
+      <div className="content d-flex">
+        <SideBar />
+        <MainContent>
+          <header className="editor-header">
+            <div className="editor-header-content d-flex justify-content-between">
+              <IoArrowBack />
+            </div>
+          </header>
+          <div className="editor container-fluid">
+            <div className="editor-content" contentEditable>
+              hello world
+            </div>
+          </div>
+        </MainContent>
+      </div>
+    </div>
+  );
+};
 
-export default NoteEditor
+export default NoteEditor;
