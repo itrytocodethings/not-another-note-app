4f4b9369e6e72f23169fab0e519bd8845af6957d
itrytocodethings <imwaynebailey@gmail.com>
2022-05-01 22:39:06 -0400

    added elements/buttons to editor

--- src/js/Pages/NoteEditor.jsx
+++ src/js/Pages/NoteEditor.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import SideBar from "../Components/Sidebar";
 import MainContent from "../Components/MainContent";
 
-import '../../assets/css/editor.css'
-import {IoArrowBack} from 'react-icons/io5';
+import "../../assets/css/editor.css";
+import { IoArrowBack } from "react-icons/io5";
+import { BsEye, BsEyeSlash, BsInfoCircle } from "react-icons/bs";
+import { HiOutlineDotsCircleHorizontal } from "react-icons/hi";
+import { Link } from "react-router-dom";
 
 const NoteEditor = () => {
   return (
@@ -12,12 +15,30 @@ const NoteEditor = () => {
         <SideBar />
         <MainContent>
           <header className="editor-header">
-            <div className="editor-header-content d-flex justify-content-between">
-              <IoArrowBack />
+            <div className="editor-header-content d-flex justify-content-between px-3 py-3">
+              <div className="editor-right d-flex">
+                <Link to="/">
+                  {" "}
+                  <button className="btn btn-primary">
+                    <IoArrowBack />
+                  </button>
+                </Link>
+              </div>
+              <div className="editor-right d-flex">
+                <button className="btn btn-primary">
+                  <BsEye />
+                </button>
+                <button className="btn btn-primary">
+                  <BsInfoCircle />
+                </button>
+                <button className="btn btn-primary">
+                  <HiOutlineDotsCircleHorizontal />
+                </button>
+              </div>
             </div>
           </header>
           <div className="editor container-fluid">
-            <div className="editor-content" contentEditable>
+            <div className="editor-content" contentEditable={true}>
               hello world
             </div>
           </div>
