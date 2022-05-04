a95524a41cea8df860d04b7d43e8720e0b9b15da
itrytocodethings <imwaynebailey@gmail.com>
2022-05-03 20:31:45 -0400

    added markdown support in notes with marked.js

--- src/js/Pages/NoteEditor.jsx
+++ src/js/Pages/NoteEditor.jsx
@@ -1,5 +1,5 @@
 //react utils
-import React, { useState, useRef, useContext } from "react";
+import React, { useState, useRef, useContext, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 //styles/icons/etc
@@ -12,6 +12,7 @@ import { HiOutlineDotsCircleHorizontal } from "react-icons/hi";
 import SideBar from "../Components/Sidebar";
 import MainContent from "../Components/MainContent";
 import ContentEditable from "react-contenteditable";
+import { marked } from "marked";
 
 // store
 import { Context } from "../store/Context";
@@ -22,6 +23,9 @@ const NoteEditor = (props) => {
   const note = loc.state.note;
   //using ref because useState doesn't work with ContentEditable package. per docs use 'useRef' instead.
   const text = useRef(note.body);
+  let textMD = useRef(marked.parse(text.current));
+  const editor = useRef(null);
+  const [showMD, setShowMD] = useState(false);
 
   return (
     <div className="editor-container">
@@ -39,7 +43,13 @@ const NoteEditor = (props) => {
                 </Link>
               </div>
               <div className="editor-right d-flex">
-                <button className="btn btn-primary">
+                <button
+                  className="btn btn-primary"
+                  onClick={() => {
+                    //testing show MD
+                    showMD ? setShowMD(false) : setShowMD(true);
+                  }}
+                >
                   <BsEye />
                 </button>
                 <button className="btn btn-primary">
@@ -54,15 +64,21 @@ const NoteEditor = (props) => {
           <div className="editor container-fluid">
             <div className="editor-content py-3">
               <ContentEditable
+                innerRef={editor}
                 className="editor edit-field px-2"
-                html={text.current}
+                html={!showMD ? text.current : textMD.current}
+                disabled={!showMD ? false : true}
                 onChange={(e) => {
                   //sets text ref .current with what is input into the editor.
-                  text.current = e.target.value; 
+                  text.current = e.target.value;
+                  //used editor ref to get innerText from editor so markdown would format correctly, innerText not defined on ContentEditable package component.
+                  textMD.current = marked.parse(editor.current.innerText);
                 }}
-                onBlur={(e) => { //when the editor is unFocus
+                onBlur={(e) => {
+                  //when the editor is unFocus
                   //using innerText to get value without HTML tags.
-                  let plainText = e.target.innerText; 
+                  text.current;
+                  let plainText = e.target.innerText;
                   actions.saveNote(loc.state.index, text.current, plainText);
                 }}
               />
